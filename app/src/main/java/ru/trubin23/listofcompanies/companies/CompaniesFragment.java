509e0bfc3847603cc6540ad05017e9ab2a563859
Andrey Trubin <trubin23@gmail.com>
2018-03-25 21:57:29 +0500

    increased text size

--- app/src/main/java/ru/trubin23/listofcompanies/companies/CompaniesFragment.java
+++ app/src/main/java/ru/trubin23/listofcompanies/companies/CompaniesFragment.java
@@ -1,15 +1,24 @@
 package ru.trubin23.listofcompanies.companies;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.jaredrummler.materialspinner.MaterialSpinner;
+import com.jaredrummler.materialspinner.MaterialSpinnerAdapter;
 
 import java.util.List;
 
@@ -61,17 +70,30 @@ public class CompaniesFragment extends Fragment implements CompaniesContract.Vie
 
     @Override
     public void setAddress(@NonNull String address) {
-        mCompanyAddress.setText(address);
+        String addressText = String.format("<b>%s:</b> %s",
+                getResources().getString(R.string.company_address), address);
+        mCompanyAddress.setText(Html.fromHtml(addressText));
     }
 
     @Override
     public void setInn(@NonNull String inn) {
-        mCompanyInn.setText(inn);
+        String innText = String.format("<b>%s:</b> %s",
+                getResources().getString(R.string.company_inn), inn);
+        mCompanyInn.setText(Html.fromHtml(innText));
     }
 
     @Override
     public void showCompanies(@NonNull List<Company> companies) {
-        mSpinner.setItems(companies);
+        mSpinner.setAdapter(new MaterialSpinnerAdapter<Company>(getContext(), companies) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                TextView textView = (TextView) super.getView(position, convertView, parent);
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                        getResources().getDimensionPixelSize(R.dimen.basic_text_size));
+                return textView;
+            }
+        });
+
         Company company = (Company) mSpinner.getItems().get(mSpinner.getSelectedIndex());
         mPresenter.selectedCompany(company);
     }
